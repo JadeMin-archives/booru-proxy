31d22c84c44c96cf4e5a8e9cfb16621bb4970211
JadeMin <36400787+JadeMin@users.noreply.github.com>
2023-05-21 06:47:24 +0900

    Create sugall.ts

---
+++ sugall.ts
@@ -0,0 +1,35 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+console.clear();
+
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const { hostname, pathname, search } = new URL(request.url);
+
+		const response = fetch(`https://booru.sugall.com${pathname}${search}`, {
+			...request,
+			cf: {
+				cacheTtl: 31536000,
+				cacheEverything: true
+			}
+		});
+		
+
+		//return response;
+		return new HTMLRewriter()
+			.on("article.withleft > section#commentlistimage", {
+				element(element) {
+					element.remove();
+				}
+			})
+			.transform(await response);
+	}
+};
